87081794db552cef6adbc8d83b4a4283554e7d71
DiademCharm-crypto <demshots015@gmail.com>
2026-09-02 00:15:32 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const featuredMovies = [
   { 
     id: "movie-10", 
-    title: "Masters of the Universe", 
+    title: "Rey Fortuno: Gamayg Tudlo", 
     description: "After being separated for 15 years, the Sword of Power leads Prince Adam back to Eternia, where he discovers his home shattered under the fiendish rule of Skeletor.",
-    poster: "https://imgs.search.brave.com/h9zgQg2PUEtOhRulETUtiYpN3z4hS3XUXJ3XXA_yp4w/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2MzL2Nk/LzZiL2MzY2Q2YjI3/YzRkMDNiNjhhM2Ez/ZDk2NGZkNTkzY2Rk/LmpwZw", 
-    backdrop: "https://images.static-bluray.com/movies/covers/414974_large.jpg?t=1785516327",
+    poster: "https://gcdnb.pbrd.co/images/hP7E46SmNmjU.jpg", 
+    backdrop: "https://gcdnb.pbrd.co/images/hP7E46SmNmjU.jpg",
     embedUrl: "https://www.youtube.com/embed/5PSNL1qE6VY" 
   },
   { 
